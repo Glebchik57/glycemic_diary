801e73e80b691f7a27f26589e1d9bb0c3bfdce88
go.sevostyanov <go.sevostyanov@tensor.ru>
2025-10-31 16:23:22 +0200

    serializers were created

---
+++ glecemic_diary/users/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from glecemic_diary.diary.serializers import DiarySerializer
+from glecemic_diary.users.models import Patient, Guest
+
+
+class GuestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Guest
+        fields = (
+            'name',
+            'surname',
+            'email',
+            'patients'
+        )
+
+
+class PatientSerializer(serializers.ModelSerializer):
+    diary = DiarySerializer()
+    guests = GuestSerializer(many=True)
+    class Meta:
+        model = Patient
+        fields = (
+            'name',
+            'surname',
+            'email',
+            'insulin',
+            'diary'
+        )
