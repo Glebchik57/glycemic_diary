9a957ed1ddec6751defc85c2b8058ef7bcb63214
go.sevostyanov <go.sevostyanov@tensor.ru>
2025-10-31 16:22:45 +0200

    viewsets were created

--- glecemic_diary/diary/views.py
+++ glecemic_diary/diary/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from glecemic_diary.diary.models import Diary, Note
+from glecemic_diary.diary.serializers import DiarySerializer, NoteSerializer
+
+
+class DiaryViewSet(viewsets.ModelViewSet):
+    queryset = Diary.objects.all()
+    serializer_class = DiarySerializer
+
+
+class NoteViewSet(viewsets.ModelViewSet):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
