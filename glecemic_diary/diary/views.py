81bf0e7ef7e925deb73aade7dc6ba8f9c25e671f
go.sevostyanov <go.sevostyanov@tensor.ru>
2025-11-01 14:20:40 +0200

    methides for owner and guestes were added

--- glecemic_diary/diary/views.py
+++ glecemic_diary/diary/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Diary, Note
 from .serializers import DiarySerializer, NoteSerializer
 
 
-class DiaryViewSet(viewsets.ModelViewSet):
+class DiaryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Diary.objects.all()
     serializer_class = DiarySerializer
+
+    def get_queryset(self):
+        return Diary.objects.filter(patient=self.request.user)
+
+    @action(methods=['get'], detail=False)
+    def read_all_patient_diary_by_guest(self):
+        diaries = Diary.objects.filter(patient__in=self.request.user.patients)
+        serializer = DiarySerializer(diaries, many=True)
+        return Response(serializer.data)
 
 
 class NoteViewSet(viewsets.ModelViewSet):
